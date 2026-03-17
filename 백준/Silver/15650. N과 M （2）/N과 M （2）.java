ceebeaf7f2f301bf20e4cd03cf71d9b7be903526
rumormela <rumormela@gmail.com>
2026-03-17 21:05:01 +0900

    [Silver III] Title: N과 M (2), Time: 68 ms, Memory: 11568 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M;
+    static boolean[] visited;
+    static int[] arr;
+
+    static void dfs(int depth, int start) {
+        if(depth == M) {
+
+            for(int i=0;i<M;i++) {
+                System.out.print(arr[i]+" ");
+            }
+            System.out.println();
+
+            return;
+        }
+
+        for(int i=start;i<=N;i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                arr[depth] = i;
+                dfs(depth+1, i);
+                arr[depth] = i;
+                visited[i] = false;
+            }
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        visited = new boolean[N+1];
+        arr = new int[M];
+        dfs(0,1);
+    }
+}
