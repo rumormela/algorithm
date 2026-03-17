bab3a27cd6b3629c981a50a2ef20a5d7dac55c74
rumormela <rumormela@gmail.com>
2026-03-17 21:38:27 +0900

    [Silver III] Title: N과 M (6), Time: 68 ms, Memory: 11584 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M;
+    static boolean[] visited;
+    static int[] arr;
+    static int[] result;
+    static StringBuilder sb = new StringBuilder();
+
+    static void dfs(int depth, int start) {
+        if(depth == M) {
+
+            for(int i=0;i<M;i++) {
+                sb.append(result[i]).append(" ");
+            }
+            sb.append("\n");
+
+            return;
+        }
+
+        for(int i=start;i<N;i++) {
+            if (!visited[i]) {
+                result[depth] = arr[i];
+                visited[i] = true;
+                dfs(depth+1, i+1);
+                result[depth] = 0;
+                visited[i] = false;
+            }
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        visited = new boolean[N];
+        arr = new int[N];
+        result = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        dfs(0,0);
+        System.out.println(sb.toString());
+    }
+}
